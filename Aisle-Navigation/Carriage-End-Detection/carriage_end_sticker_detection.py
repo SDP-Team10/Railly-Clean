22264fbf742f026198ea4bddf939384162ca4cec
caitlin-mcdougall <caitlin.mcdougall@hotmail.co.uk>
2021-01-28 10:43:21 +0000

    #1 Add camera as parameter to primary function

--- Aisle-Navigation/Carriage-End-Detection/carriage_end_sticker_detection.py
+++ Aisle-Navigation/Carriage-End-Detection/carriage_end_sticker_detection.py
@@ -1,4 +1,4 @@
-"""lidar_sensing controller."""
+"""Carriage_End controller."""
 import cv2
 import numpy as np
 # You may need to import some classes of the controller module. Ex:
@@ -10,7 +10,8 @@ robot = Robot()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
-
+camera = robot.getDevice('camera1')
+camera.enable(timestep)
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -18,25 +19,17 @@ timestep = int(robot.getBasicTimeStep())
 #  ds = robot.getDistanceSensor('dsname')
 #  ds.enable(timestep)
 
-camera = robot.getDevice('camera1')
-camera.enable(1)
 
 
 def blob_detection(img):
     mask = cv2.inRange(img, (0.5, 0, 0), (0.65, 0.3, 0.1))
-    cv2.imshow("Window 3",mask)
-    cv2.waitKey(1000)
     kernel = np.ones((1, 1), np.uint8)
     mask = cv2.dilate(mask, kernel, iterations=3)
     kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel2, iterations=2)
-    cv2.imshow("Window 9",mask)
-    cv2.waitKey(2000)
     contours, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     nu = np.zeros(img.shape)
     nu = cv2.drawContours(nu, contours, -1, (0,0,255), 1)
-    cv2.imshow("Window 4",nu)
-    cv2.waitKey(4000)
     cv2.destroyAllWindows()
     return mask
 
@@ -47,23 +40,13 @@ def matchsticker(image):
     thresh = cv2.bitwise_not(thresh)
     kernel = np.ones((5, 5), np.uint8)
     thresh = cv2.dilate(thresh, kernel, iterations=3)
-    cv2.imshow("Window 4",thresh)
-    cv2.waitKey(4000)
     kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
     thresh= cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel2, iterations=3)
-    cv2.imshow("Window 8",thresh)
-    cv2.waitKey(4000)
-    print(type(thresh))
-    print(thresh.dtype)
     template_contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     cnt1 = template_contours[0]
-    cv2.imshow("Window 10",image)
-    cv2.waitKey(4000)
     contours, _ = cv2.findContours(image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     nu = np.zeros(image.shape)
     nu = cv2.drawContours(nu, contours, -1, (0,0,255), 1)
-    cv2.imshow("Window 4",nu)
-    cv2.waitKey(2000)
     if len(contours) == 0:
         return False
     cnt2 = contours[0]
@@ -89,8 +72,8 @@ def remove_illumination(image):
 
 
 # Press the green button in the gutter to run the script.
-def is_carriage_end():
-    img = camera.getImageArray()
+def is_carriage_end(cam):
+    img = cam.getImageArray()
     gbr_img = []
     for a in range(len(img)):
         temp = []
@@ -98,27 +81,29 @@ def is_carriage_end():
             temp.append([img[b][a][2],img[b][a][1],img[b][a][0]])
         gbr_img.append(temp)
     img = np.array(gbr_img,np.uint8)
-    cv2.imshow("Window 1", img)
-    cv2.waitKey(4000)
     img = remove_illumination(img)
-    cv2.imshow("Window 2", img)
-    cv2.waitKey(4000)
     cv2.destroyAllWindows()
     blob = blob_detection(img)
-    return matchsticker(blob)
+    matches = matchsticker(blob)
+    cv2.destroyAllWindows()
+    return matches
+count = 0
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
-        
-       
+    if count == 100:
+        print(is_carriage_end(camera))
+    count +=1
+
     # Process sensor data here.
 
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
-    
+
     pass
 
-# Enter here exit cleanup code.
+# Enter here exit cleanup code.r here exit cleanup code.
+
