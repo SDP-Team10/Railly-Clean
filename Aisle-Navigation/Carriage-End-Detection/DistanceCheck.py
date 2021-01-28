9166fc0938a6672c2843ef26a89ad6aefcf4d546
stodimp <stodulka.mate@gmail.com>
2021-01-28 13:33:33 +0100

    Create DistanceCheck.py
    DistanceCheck helper functions
    
    Create helper functions to initialize and check distance sensors

---
+++ Aisle-Navigation/Carriage-End-Detection/DistanceCheck.py
@@ -0,0 +1,28 @@
+# Distance sensor helper functions for railly Clean
+from controller import Robot, DistanceSensor
+
+# Distance sensor initilazation
+def initDist(robot, dsNum, dsNames, timestep):
+    """ function initDist
+    :param robot: webots robot object
+    :param dsNum: number of sensors on the robot
+    :param dsName: names of the 'physical' distance sensor on the webots robot
+    :param timestep: Time step in the webots controller file
+    :return: list of enabled distance sensor objects
+    """
+    distanceSensors = []
+    for i in range(dsNum):
+        distanceSensors.append(robot.getDevice(dsNames[i]))
+        distanceSensors[i].enable(timestep)
+    return distanceSensors
+
+#Checking for obstacles
+def shouldStop(distanceSensor):
+    """function shouldStop
+    :param distanceSensor: distancesensor that needs to be checked for objects in the way
+    :return: boolean if distance is <40cm, return true, otherwise false
+    """
+    if(distanceSensor.getValue<4e+4): #0.4 is placeholder, need to set up "lookupTable field
+        return True
+    else:
+        return False
