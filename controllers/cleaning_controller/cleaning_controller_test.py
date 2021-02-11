261d4f6357ef2275a29441f3ac397306584c8827
anh-ng-21 <77205161+anh-ng-21@users.noreply.github.com>
2021-02-11 18:47:31 +0700

    Fix test controller to work with arm_controller and side_check #13

--- controllers/cleaning_controller/cleaning_controller_test.py
+++ controllers/cleaning_controller/cleaning_controller_test.py
@@ -8,14 +8,14 @@ sys.path.append(os.path.abspath(os.path.join("..", "..")))
 from libraries import sticker_detection as vc
 from libraries import side_check as sc
 from libraries import move_lib as mc
-from libraries import arm_controller as ac  # to change
+from libraries import arm_controller as ac
 from controller import Robot
 
 
 TIME_STEP = 32  # this or robot.getBasicTimeStep()
 STOP_THRESHOLD = 0.6
-TABLE_WIDTH = 1
-HEAD_WIDTH = 0.3
+TABLE_WIDTH = 1  # parameter
+HEAD_WIDTH = 0.3  # parameter
 CLEAN_ATTEMPTS = math.ceil(TABLE_WIDTH // HEAD_WIDTH)
 
 
@@ -42,6 +42,7 @@ class CleaningController(object):
         # arm motors' position sensors?
         self.left_motor = self.robot.getDevice("wheel_left_joint")
         self.right_motor = self.robot.getDevice("wheel_right_joint")
+        self.arm_controller = ac.ArmController(self.robot)
 
     # Arm's motors
     def init_arm(self):
@@ -65,9 +66,10 @@ class CleaningController(object):
 
     def clean_table(self):
         mc.stop(self.robot)
-        ac.sweep(self.robot)
-        ac.wipe(self.robot)
-        ac.next_iteration(self.robot)
+        self.arm_controller.set_sweeping_action()
+        self.arm_controller.sweep_action()
+        self.arm_controller.tuck_in_action()
+        # ac.wipe(self.robot)
     
     @staticmethod
     def next_row(table_check_l, table_check_r):
@@ -80,15 +82,15 @@ class CleaningController(object):
 if __name__ == "__main__":
     controller = CleaningController()
     robot, dist_sensors = controller.robot, controller.distance_sensors
-    table_check_l, table_check_r = sc.SideCheck(), sc.SideCheck()
+    table_check_l, table_check_r = sc.SideCheck(robot), sc.SideCheck(robot)
     table_length_l, table_length_r = None, None
     table_detected, side = False, 'f'
 
     # Assume robot is already centered
     while controller.robot.step(controller.time_step) != -1:
         if not table_detected:
-            table_length_l = table_check_l.side_check(robot, dist_sensors[2])
-            table_length_r = table_check_r.side_check(robot, dist_sensors[3])
+            table_length_l = table_check_l.side_check(dist_sensors[2])
+            table_length_r = table_check_r.side_check(dist_sensors[3])
             
             if table_length_l or table_length_r:  # if not None -> table detected
                 table_detected = True
