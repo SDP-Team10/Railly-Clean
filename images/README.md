fe34466965b0ad34c403a4f9af7760661cb6c500
Apurv Mishra <me@apurvmsihra.xyz>
2021-02-19 15:47:12 +0000

    add training notebbok, saved models & lib function

---
+++ images/README.md
@@ -0,0 +1,5 @@
+# Datasets
+
+### Trash Classifer
+
+- Dataset of ~1000 not trash and 100 trash images [here](https://drive.google.com/file/d/1_apKEy37yC0cWZ9ueKgcYjlOqBRqat6-/view?usp=sharing)
---
+++ libraries/classify_trash.py
@@ -0,0 +1,29 @@
+import numpy as np
+# if running on RPi
+# from tflite_runtime.interpreter import Interpreter
+# else
+import tensorflow as tf
+import tensorflow.lite as tfl
+
+# The image should be read like this:
+# camera_data = camera.getImage();
+def classify_trash(camera_data: str) -> tuple[str, float]:
+    image = tf.io.decode_jpeg(camera_data, channels=3)
+    labels = ("not_trash", "trash")
+    interpreter = tfl.Interpreter("../ml_models/model_quant.tflite")
+    interpreter.allocate_tensors()
+    _, height, width, _ = interpreter.get_input_details()[0]['shape']
+    image = tf.image.resize(image, [height, width])
+    input_img = np.expand_dims(image, axis=0).astype(np.uint8)
+    
+    output_details = interpreter.get_output_details()[0]
+    input_index = interpreter.get_input_details()[0]["index"]
+    output_index = interpreter.get_output_details()[0]["index"]
+
+    interpreter.set_tensor(input_index, input_img)
+    interpreter.invoke()
+    
+    predictions = np.squeeze(interpreter.get_tensor(output_index))
+    scale, zero_point = output_details['quantization']
+    predictions = scale * (predictions - zero_point)
+    return max(zip(labels, predictions), key=lambda x: x[1])
