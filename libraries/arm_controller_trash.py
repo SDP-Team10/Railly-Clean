d7724aa51dc9f0510995ab019e61861e224ff016
caitlin-mcdougall <caitlin.mcdougall@hotmail.co.uk>
2021-03-04 20:02:03 +0000

    three arm kinematics

--- libraries/arm_controller_trash.py
+++ libraries/arm_controller_trash.py
@@ -16,12 +16,14 @@ class ArmController(object):
             "base_sensor",
             "sec_1_sensor",
             "sec_2_sensor",
+            "sec_3_sensor",
             "head_sensor"
         ]
         self.rm_names = [
             "base_motor",
             "sec_1_motor",
             "sec_2_motor",
+            "sec_3_motor",
             "head_motor"
         ]
         self.position_sensors = self.init_ps()
@@ -58,45 +60,47 @@ class ArmController(object):
         d_x = height
         y_step = 0.1 * d_y
         x_step = 0.05 * d_x
+        self.rotational_motors[2].setPosition(0.0)
+        self.rotational_motors[2].setVelocity(0.0)
         while d_y > 0.2:
             print("kine boi")
-            dq = kinematics.brute_force(d_x + 0.1, d_y, 0.78, 0.7, round(self.position_sensors[1].getValue(), 2),
-                                        round(self.position_sensors[2].getValue(), 2))
+            dq = kinematics.brute_force(d_x + 0.1, d_y, 1.0, 0.5, round(self.position_sensors[1].getValue(), 2),
+                                        round(self.position_sensors[3].getValue(), 2))
             new_desired = kinematics.kinematics(dq[0],dq[1], 0.78, 0.7)
-            dq = kinematics.brute_force3(new_desired[3]-0.1, new_desired[7], 0.78, 0.7, 0.1, dq[0], dq[1],
-                                         round(self.position_sensors[3].getValue(), 2))
+            dq = kinematics.brute_force3(new_desired[3]-0.1, new_desired[7], 1.0, 0.5, 0.1, dq[0], dq[1],
+                                         round(self.position_sensors[4].getValue(), 2))
             d_y -= y_step
             print(d_y)
             print(d_x)
             self.rotational_motors[1].setPosition(dq[0])
-            self.rotational_motors[2].setPosition(dq[1])
-            self.rotational_motors[3].setPosition(dq[2])
+            self.rotational_motors[3].setPosition(dq[1])
+            self.rotational_motors[4].setPosition(dq[2])
             self.rotational_motors[1].setVelocity(0.2)
-            self.rotational_motors[2].setVelocity(0.8)
             self.rotational_motors[3].setVelocity(0.8)
+            self.rotational_motors[4].setVelocity(0.8)
             print('dqw[0]: ', dq[0])
             print('dqw[1]: ', dq[1])
             while self.robot.step(self.time_step) != -1:
                 print('starting with')
                 print("1=", self.position_sensors[1].getValue())
-                print("2=", self.position_sensors[2].getValue())
+                print("2=", self.position_sensors[3].getValue())
                 print('want')
                 print("1=", dq[0])
                 print("2=", dq[1])
                 print('-------------------------------')
-                self.last_4_positions.append([self.position_sensors[1].getValue(), self.position_sensors[2].getValue(),
-                                              self.position_sensors[3].getValue()])
+                self.last_4_positions.append([self.position_sensors[1].getValue(), self.position_sensors[3].getValue(),
+                                              self.position_sensors[4].getValue()])
                 if self.is_stationary(self.last_4_positions):
                     d_x = height + 0.01
                     print("stationary")
                     break
                 if (round(self.position_sensors[1].getValue(), 2) == round(dq[0], 2) and
-                        round(self.position_sensors[2].getValue(), 1) == round(dq[1], 1) and
-                        round(self.position_sensors[3].getValue(), 1) == round(dq[2], 1)):
+                        round(self.position_sensors[3].getValue(), 1) == round(dq[1], 1) and
+                        round(self.position_sensors[4].getValue(), 1) == round(dq[2], 1)):
 
                     print('Goal1: ', round(self.position_sensors[1].getValue(), 2))
-                    print('Goal2: ', round(self.position_sensors[2].getValue(), 1))
-                    print('Goal3: ', round(self.position_sensors[3].getValue(), 1))
+                    print('Goal2: ', round(self.position_sensors[3].getValue(), 1))
+                    print('Goal3: ', round(self.position_sensors[4].getValue(), 1))
                     break
         return
 
@@ -137,9 +141,9 @@ class ArmController(object):
             print('d_y now set to 1.4')
             d_y = 1.4
         d_x = height
-        k = kinematics.brute_force(d_x + 0.1, d_y, 0.78, 0.7)
-        new_desired = kinematics.kinematics(k[0], k[1], 0.78, 0.7)
-        k = kinematics.brute_force3(new_desired[3]-0.1, new_desired[7], 0.78, 0.7, 0.1, k[0], k[1])
+        k = kinematics.brute_force(d_x + 0.1, d_y, 1.0, 0.5)
+        new_desired = kinematics.kinematics(k[0], k[1], 1.0, 0.5)
+        k = kinematics.brute_force3(new_desired[3]-0.1, new_desired[7], 1.0, 0.5, 0.1, k[0], k[1])
         print('k: ', k)
         # pos1 = 1.44
         pos1 = k[0]
@@ -152,30 +156,32 @@ class ArmController(object):
         vel2 = 1
         vel3 = 1
         self.rotational_motors[1].setPosition(pos1)
-        self.rotational_motors[2].setPosition(pos2)
-        self.rotational_motors[3].setPosition(pos3)
+        self.rotational_motors[2].setPosition(0.0)
+        self.rotational_motors[3].setPosition(pos2+1.57)
+        self.rotational_motors[4].setPosition(pos3)
         self.rotational_motors[1].setVelocity(vel1)
-        self.rotational_motors[2].setVelocity(vel2)
-        self.rotational_motors[3].setVelocity(vel3)
+        self.rotational_motors[2].setVelocity(1)
+        self.rotational_motors[3].setVelocity(vel2)
+        self.rotational_motors[4].setVelocity(vel3)
         # # print("TORQUE", self.rotational_motors[1].getAvailableTorque())
         # # print("HIGHEST_TORQUE", self.rotational_motors[1].getMaxTorque())
         # # print("FORCE", self.rotational_motors[1].getAvailableForce())
         # # print("MAX FORCE", self.rotational_motors[1].getAvailableTorque())
         while self.robot.step(self.time_step) != -1:
             print("1", self.position_sensors[1].getValue())
-            print("2", self.position_sensors[2].getValue())
+            print("2", self.position_sensors[3].getValue())
             self.last_4_positions.append([self.position_sensors[1].getValue(),self.position_sensors[2].getValue(), self.position_sensors[3].getValue()])
             if (round(self.position_sensors[1].getValue(), 2) == round(pos1, 2) and
-                    round(self.position_sensors[2].getValue(), 2) == round(pos2, 2) and
-                    round(self.position_sensors[2].getValue(), 2) == round(pos2, 2)) or \
+                    round(self.position_sensors[3].getValue(), 2) == round(pos2, 2) and
+                    round(self.position_sensors[3].getValue(), 2) == round(pos2, 2)) or \
                     self.is_stationary(self.last_4_positions):
                 print("bingo, Setting done")
                 print(self.rotational_motors[1].getVelocity())
                 print(self.rotational_motors[2].getVelocity())
                 self.rotational_motors[1].setVelocity(0.0)
-                self.rotational_motors[2].setVelocity(0.0)
                 self.rotational_motors[3].setVelocity(0.0)
-                return kinematics.kinematics3joint(self.position_sensors[1].getValue(),self.position_sensors[2].getValue(),self.position_sensors[3].getValue(),0.78, 0.7, 0.1,)[3]
+                self.rotational_motors[4].setVelocity(0.0)
+                return kinematics.kinematics3joint(self.position_sensors[1].getValue(),self.position_sensors[3].getValue(),self.position_sensors[4].getValue(),1.0, 0.5, 0.1,)[3]
 
     def sweep(self, distance_to_wall):
         # It assumes the arm is tucked in and try to do the sweep action from reaching the
--- libraries/bin_controller.py
+++ libraries/bin_controller.py
@@ -11,16 +11,16 @@ class BinController(object):
         self.closed_pos = 0
         self.opened_pos = -1
 
-    def open_bin(self):
-        self.bin_motor.setVelocity(1)  # 1 rad/s
+    def open_bin(self): # 1 rad/s
         self.bin_motor.setPosition(self.opened_pos)
-        while self.bin_sensor.getValue() != self.opened_pos:
+        self.bin_motor.setVelocity(1)
+        while round(self.bin_sensor.getValue(),1) != self.opened_pos:
             self.robot.step(self.time_step)
 
     def close_bin(self):
         self.bin_motor.setVelocity(1)  # 1 rad/s
         self.bin_motor.setPosition(self.closed_pos)
-        while self.bin_sensor.getValue() != self.closed_pos:
+        while round(self.bin_sensor.getValue(),1) != self.closed_pos:
             self.robot.step(self.time_step)
     
     # use touch sensor?
