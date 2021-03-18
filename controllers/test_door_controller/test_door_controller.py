ab794e7bb528a29a09d5a9678c0ee1175b9505e1
Daniel Spencer <47607407+danieljspencer@users.noreply.github.com>
2021-03-18 10:46:34 +0000

    Created models for demo 4

---
+++ controllers/test_door_controller/test_door_controller.py
@@ -0,0 +1,43 @@
+"""test_door_controller controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+slide_motor = robot.getDevice("slide_motor")
+button_sensor = robot.getDevice("button_sensor")
+
+button_sensor.enable(1) #sets the sampling period
+
+# 1.0 if a collision is detected and 0.0 otherwise
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getMotor('motorname')
+#  ds = robot.getDistanceSensor('dsname')
+#  ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    #print(button_sensor.getValue())
+    button_value = button_sensor.getValue()
+    if (button_value == 1):
+        slide_motor.setPosition(1.1)
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    pass
+
+# Enter here exit cleanup code.
