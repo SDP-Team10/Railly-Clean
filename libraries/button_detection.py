9c01025a8794b0e5166f0c226fe037f3dafbdd8a
stodimp <stodulka.mate@gmail.com>
2021-03-26 16:17:31 +0100

    Fix centering controller
    And some button detection magic

--- libraries/button_detection.py
+++ libraries/button_detection.py
@@ -24,22 +24,22 @@ def button_match(
     kp2, des2 = finder.detectAndCompute(image_gray, None)
     # des1, des2 = np.float32(des1), np.float32(des2)
     kp_img = cv2.drawKeypoints(image_gray, kp2, None, color=(0, 255, 0), flags=0)
-    cv2.imshow('ORB', kp_img)
-    cv2.waitKey(0)
+    # cv2.imshow('ORB', kp_img)
+    # cv2.waitKey(0)
 
     FLANN_INDEX_LSH = 6
     index_params = dict(algorithm=0, trees=5)
     # index_params= dict(algorithm=FLANN_INDEX_LSH, table_number=12, key_size=20, multi_probe_level=2)
     search_params = dict(checks=50)
-    
+
     flann = cv2.FlannBasedMatcher(index_params, search_params)
     matches = flann.knnMatch(des1, des2, k=2)
 
-    # Low resolution so ratio = 0.85 
+    # Low resolution so ratio = 0.85
     good = []
     for match in matches:
         if len(match) > 1:
-            m, n = match    
+            m, n = match
             if m.distance < 1.0 * n.distance:
                 good.append(m)
 
@@ -58,7 +58,9 @@ def button_match(
             [[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]], dtype=np.float32
         ).reshape(-1, 1, 2)
         dst = cv2.perspectiveTransform(pts, M)
-        image_match = cv2.polylines(image, dst.astype(np.int32), True, 0, 3, cv2.LINE_AA)
+        image_match = cv2.polylines(
+            image, dst.astype(np.int32), True, 0, 3, cv2.LINE_AA
+        )
         cv2.imshow("match", image_match)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
